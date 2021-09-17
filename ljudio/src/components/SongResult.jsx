08f3ca5b0b28af57ddb8bae5fe9c58158fce3909
Jenny Wong <jennywong567@gmail.com>
2021-09-17 09:44:56 +0200

    fixed button styling

--- ljudio/src/components/SongResult.jsx
+++ ljudio/src/components/SongResult.jsx
@@ -1,29 +1,41 @@
-import React, {useState, useContext} from "react";
+import React, { useState, useContext } from "react";
 import PlayerControls from "./PlayerControls";
 import { StoreContext } from "../utils/store";
-import { useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom";
 
 import "./SongResult.css";
 
 function SongResult({ result }) {
   const url = result.thumbnails[0].url;
-  const {currentSongId: [currentSongId, setCurrentSongId] } = useContext(StoreContext)
-  let history = useHistory()
+  const {
+    currentSongId: [currentSongId, setCurrentSongId],
+  } = useContext(StoreContext);
+  let history = useHistory();
 
-  const songId = (id) =>{
-    setCurrentSongId(id)
-  }
-  
+  const songId = (id) => {
+    setCurrentSongId(id);
+  };
+
   const toSongLink = () => {
-    history.push('/song/' + result.videoId);
-  }
+    history.push("/song/" + result.videoId);
+  };
+
+  //removed old button from song-info
+  // <button onClick={(e) => songId(result.videoId)}>
+  //     <i className="fas fa-play"></i>
+  //     {/* <i className="fas fa-pause"></i> */}
+  //   </button>
 
   return (
     <div className="result-div">
-      <img src={url} alt="" style={{
-          borderRadius:"10px",
-          width:"60px"
-          }} />
+      <img
+        src={url}
+        alt=""
+        style={{
+          borderRadius: "10px",
+          width: "60px",
+        }}
+      />
       <div className="song-info">
         <p>
           Artist:
@@ -33,14 +45,12 @@ function SongResult({ result }) {
           Title: <span style={{ fontWeight: "bold" }}>{result.name}</span>
         </p>
       </div>
-      <button onClick={(e) => songId(result.videoId)}>
-          <i className="fas fa-play"></i>
-          {/* <i className="fas fa-pause"></i> */}
-        </button>
       <PlayerControls id={result.videoId} />
-      <button className="btn">
-        <i className="fas fa-share" onClick={toSongLink}></i>
-      </button>
+      <div>
+        <button className="btn btn-toLink" onClick={toSongLink}>
+          <i className="fas fa-share"></i>
+        </button>
+      </div>
     </div>
   );
 }
